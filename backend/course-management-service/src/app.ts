56c8ce38b3153c24773b89d802bfe68b8dd7024f
Prashantha K.G.M <it21169908@my.sliit.lk>
2024-05-08 23:56:35 +0530

    Course management service initial node setup

---
+++ backend/course-management-service/src/app.ts
@@ -0,0 +1,36 @@
+// <reference path="global.d.ts" />
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+import cors from 'cors';
+import favicon from 'serve-favicon';
+import * as favPath from 'path';
+
+const isProduction = process.env.NODE_ENV === "production";
+const app = express();
+
+
+app.use(express.json({limit: '20mb'}));
+app.use(express.urlencoded({limit: '20mb', extended: true}));
+
+if (!isProduction) {
+    app.use(morgan("dev"));
+    app.use(cors({
+        optionsSuccessStatus: 200,
+        origin: '*',
+        allowedHeaders: ['Content-Type, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Authorization, X-Requested-With', 'Cache-Control']
+    }));
+} else {
+    app.use(morgan('combined'));
+    app.use(cors());
+}
+
+
+app.use(favicon(favPath.join(__dirname, "../resources", "favicons/favicon.ico")));
+app.use('/api/static', express.static(favPath.join(__dirname, "../resources")));
+
+app.get('/api', (req, res) => {
+    res.json("Course Management service™ API").status(200);
+});
+
+export default app;
