e816a32c32d68c4402f2e9f27385f165780f98ca
Prashantha K.G.M <it21169908@my.sliit.lk>
2024-05-10 21:11:55 +0530

    Add basic layout

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            'img.freepik.com',
+            'res.cloudinary.com',
+            'images.pexels.com',
+        ],
+    },
+};
 
 export default nextConfig;
